53eae5a93ec0e10cea6c0d544c2557afb7c7ca54
Pedro Rojas <pedro.rodrigues.rojas@gmail.com>
2022-02-22 18:31:55 -0300

    ajustando a suite home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -5,11 +5,8 @@ describe('home page', () => {
         /*cy -> cy é a variavel usada no cypress para poder acessar a API do cypress e poder usar os 
         recursos do framework*/
 
-        //cy.viewport() -> serve para mudar a resolução(tamanho) da janela que o cypress irá abrir
-        cy.viewport(1440, 900)
-
         //cy.visit() -> visita uma página
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
 
         //Verificar se achou o titulo da tela - O should é um assert
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
